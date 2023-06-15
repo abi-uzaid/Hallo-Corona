9e12797bdfa780fa48b1ded80145280182f1ac20
abiuzaid <abiuzaid1427@gmail.com>
2023-06-15 11:47:29 +0700

    perbaikan

--- server/dto/article/article_request.go
+++ server/dto/article/article_request.go
@@ -1,7 +1,7 @@
 package articledto
 
 type CreateArticleRequest struct {
-	Title    string `json:"title" gorm:"type: varchar(255)" validate:"required"`
+	Title    string `json:"title" form:"title" gorm:"type:varchar(255)" validate:"required"`
 	UserID   int    `json:"user_id" form:"user_id"`
 	Image    string `json:"image" gorm:"type: varchar(255)"`
 	Desc     string `json:"desc" gorm:"type:text" form:"desc" validate:"required"`
